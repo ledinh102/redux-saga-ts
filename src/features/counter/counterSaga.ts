f43e7e1fdb6904ae500ce41c13741669de27ab9d
Le Dinh <lenguyennhutdinh@gmail.com>
2023-07-21 20:46:09 +0700

    takeEvery and takeLasted

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,13 +1,22 @@
-import { takeEvery } from "@redux-saga/core/effects"
 import { PayloadAction } from "@reduxjs/toolkit"
-import { increment } from "./counterSlice"
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects"
+import { increment, incrementSaga, incrementSagaSuccess } from "./counterSlice"
 
-export function* log(action: PayloadAction) {
-  console.log("Log", action)
+// export function* log(action: PayloadAction) {
+//   console.log("Log", action)
+//   yield 1
+// }
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  console.log("Waiting 1s")
+  yield delay(1000)
+  console.log("Waiting done, dispatch action")
+  yield put(incrementSagaSuccess(action.payload))
 }
 
 export default function* counterSaga() {
   console.log("counter saga")
 
-  yield takeEvery(increment().type, log)
+  yield takeEvery(incrementSaga.toString(), handleIncrementSaga)
+  // yield takeLatest(incrementSaga.toString(), handleIncrementSaga)
 }
