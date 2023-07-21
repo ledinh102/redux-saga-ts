6e9c02fa77accdb8e59334561b2b456cb71c0a24
Le Dinh <lenguyennhutdinh@gmail.com>
2023-07-21 20:53:59 +0700

    write function to easy test

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,11 +1,18 @@
 import { PayloadAction } from "@reduxjs/toolkit"
-import { delay, put, takeEvery, takeLatest } from "redux-saga/effects"
+import { call, delay, put, takeEvery, takeLatest } from "redux-saga/effects"
 import { increment, incrementSaga, incrementSagaSuccess } from "./counterSlice"
+import { fetchCount } from "./counterAPI"
 
 // export function* log(action: PayloadAction) {
 //   console.log("Log", action)
 //   yield 1
 // }
+
+function* test() {
+  yield fetchCount(2)
+
+  yield call(fetchCount, 2)
+}
 
 function* handleIncrementSaga(action: PayloadAction<number>) {
   console.log("Waiting 1s")
